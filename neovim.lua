6627e38d8de2b9f261ce1331e7a274e92392fdf7
Minhaj Shafin <minhajshafin@gmail.com>
2026-05-03 11:22:05 +0600

    init

---
+++ neovim.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    name = "onedarkpro",
+    priority = 1000,
+    opts = {
+      theme = "onedark_dark",
+      options = {
+        transparency = false,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark_dark",
+    },
+  },
+}
